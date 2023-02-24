ac1e39230f5775e33013294da54a60b3b13c21d8
Muhammad Akmal <akmal@emojiit.com>
2023-02-24 11:56:11 +0200

    'mail-fix'

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -12,17 +12,22 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendContactUsEmail(userData: User) {
-    await this.mailerService.sendMail({
-      to: "info@iskander-lbc.com",
-      from: '"Support Team" <noreply@example.com>', // override default from
-      subject: userData.subject, // Subject line
-      template: './confirmation', // `.hbs` extension is appended automatically
-      context: { // ✏️ filling curly brackets with content
-        name: userData.name,
-        email: userData.email,
-        subject: userData.subject,
-        message: userData.message,
-      },
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: "info@iskander-lbc.com",
+        from: '"Support Team" <noreply@example.com>', // override default from
+        subject: userData.subject, // Subject line
+        template: './confirmation', // `.hbs` extension is appended automatically
+        context: { // ✏️ filling curly brackets with content
+          name: userData.name,
+          email: userData.email,
+          subject: userData.subject,
+          message: userData.message,
+        },
+      });
+    } catch (error) {
+      console.log(error)
+      return error
+    }
   }
 }
